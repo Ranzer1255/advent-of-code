1816150486d0804b7994e9f6dba73fdcee5409a7
ranzer <sgmaniac1255@gmail.com>
2020-12-01 16:21:04 -0600

    2020 day 1

--- src/net/ranzer/aoc/framework/Runner.java
+++ src/net/ranzer/aoc/framework/Runner.java
@@ -3,6 +3,7 @@ package net.ranzer.aoc.framework;
 import net.ranzer.aoc.y2015.day03.Day03;
 import net.ranzer.aoc.y2015.day05.Day05;
 import net.ranzer.aoc.y2015.day06.Day06;
+import net.ranzer.aoc.y2020.day01.Day01;
 
 import java.util.Date;
 
@@ -11,12 +12,12 @@ public class Runner {
 	public static void main(String[] args) {
 
 		Date startTime = new Date();
-		Day testDay = new Day06();
+		Day testDay = new Day01();
 
 
 
 		testDay.part1();
-//		testDay.part2();
+		testDay.part2();
 
 
 		Date endTime = new Date();
---
+++ src/net/ranzer/aoc/y2020/day01/Day01.java
@@ -0,0 +1,51 @@
+package net.ranzer.aoc.y2020.day01;
+
+import net.ranzer.aoc.framework.Day;
+import net.ranzer.aoc.framework.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day01 extends Day {
+
+	List<Integer> expenses = new ArrayList<>();
+
+	public Day01(){
+		input = Input.getScanner(2020,1);
+
+		while (input.hasNext()){
+			expenses.add(input.nextInt());
+		}
+	}
+
+
+	@Override
+	public void part1() {
+
+		for (int i = 0; i < expenses.size(); i++) {
+			for (int j = i+1; j < expenses.size(); j++) {
+				if(expenses.get(i) + expenses.get(j) ==2020){
+					System.out.println(expenses.get(i)*expenses.get(j));
+					break;
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void part2() {
+
+		for (int i = 0; i < expenses.size(); i++) {
+			for (int j = i+1; j < expenses.size(); j++) {
+				for (int k = j+1; k < expenses.size(); k++) {
+					if(expenses.get(i) + expenses.get(j)+ expenses.get(k) ==2020){
+						System.out.println(expenses.get(i)*expenses.get(j)*expenses.get(k));
+						break;
+					}
+				}
+			}
+		}
+
+	}
+}
