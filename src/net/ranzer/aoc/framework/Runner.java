cb16f0283fdfe85cc52e8d52f3b73e2da23df030
ranzer <sgmaniac1255@gmail.com>
2020-12-07 08:08:46 -0600

    2019 day 1

--- src/net/ranzer/aoc/framework/Runner.java
+++ src/net/ranzer/aoc/framework/Runner.java
@@ -1,5 +1,6 @@
 package net.ranzer.aoc.framework;
 
+import net.ranzer.aoc.y2019.day01.Day01;
 import net.ranzer.aoc.y2020.day05.Day05;
 import net.ranzer.aoc.y2020.day06.Day06;
 import net.ranzer.aoc.y2020.day07.Day07;
@@ -11,7 +12,7 @@ public class Runner {
 	public static void main(String[] args) {
 
 		Date startTime = new Date();
-		Day testDay = new Day07();
+		Day testDay = new Day01();
 
 
 
---
+++ src/net/ranzer/aoc/y2019/day01/Day01.java
@@ -0,0 +1,50 @@
+package net.ranzer.aoc.y2019.day01;
+
+import net.ranzer.aoc.framework.Day;
+import net.ranzer.aoc.framework.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day01 extends Day {
+
+	List<Integer> parts = new ArrayList<>();
+
+	public Day01() {
+		input = Input.getScanner(2019,1);
+		while (input.hasNext()){
+			parts.add(Integer.parseInt(input.nextLine()));
+		}
+	}
+
+	@Override
+	public void part1() {
+
+		int sum = 0;
+
+		for (int mass:parts){
+			sum+= (mass/3)-2;
+		}
+
+		System.out.println(sum);
+
+	}
+
+	@Override
+	public void part2() {
+
+		//make a copy of the the list to preserve initialized state (in case part one is run *after* part 2)
+		List<Integer> fuelCalcs = new ArrayList<>(parts);
+
+		int fuelTotal = 0;
+		for (int i = 0; i < fuelCalcs.size(); i++) {
+			int fuel = (fuelCalcs.get(i)/3)-2;
+			if (fuel>0){
+				fuelTotal += fuel;
+				fuelCalcs.add(fuel);
+			}
+		}
+
+		System.out.println(fuelTotal);
+	}
+}
