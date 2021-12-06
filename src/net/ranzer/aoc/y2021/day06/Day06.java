b680b35caa55e88a456e2e0a2e215eeeff3a010f
Ranzer <sgmaniac1255@gmail.com>
2021-12-06 07:29:28 -0600

    2021 Day 6 wip

---
+++ src/net/ranzer/aoc/y2021/day06/Day06.java
@@ -0,0 +1,26 @@
+package net.ranzer.aoc.y2021.day06;
+
+import net.ranzer.aoc.framework.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day06 extends Day {
+
+	List<Integer> fish = new ArrayList<>();
+
+
+	@Override
+	public void part1() {
+
+	}
+
+	@Override
+	public void part2() {
+
+	}
+
+	public void reset(){
+
+	}
+}
